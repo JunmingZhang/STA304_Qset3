15c2bbca5e2a55bfc889328048452f5d4b892e24
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2020-11-01 08:53:50 -0500

    my change

--- 01-data_cleaning-post-strat1.R
+++ 01-data_cleaning-post-strat1.R
@@ -16,7 +16,7 @@ library(tidyverse)
 # setwd("C:/Users/Sammi-Jo/Desktop/PS3")
 setwd(".")
 # raw_data <- read_dta("inputs/usa_00002.dta.gz")
-raw_data <- read_dta("usa_00001.dta")
+raw_data <- read_dta("../STA304/now/question_set/Qset3/raw_data/usa_00001.dta")
 
 
 # Add the labels
@@ -37,7 +37,8 @@ reduced_data <-
          citizen,
          educd,
          labforce,
-         inctot)
+         inctot,
+         perwt)
 rm(raw_data)
          
 
@@ -58,7 +59,128 @@ head(reduced_data)
 #   filter(age != "less than 1 year old") %>%
 #   filter(age != "90 (90+ in 1980 and 1990)")
 
+# create age category
 reduced_data$age <- as.integer(reduced_data$age)
+reduced_data = reduced_data %>%
+  mutate(age_group = case_when (
+    age >= 18 & age < 20 ~ '18 ~ 20',
+    age >= 20 & age < 30 ~ '20 ~ 30',
+    age >= 30 & age < 40 ~ '30 ~ 40',
+    age >= 40 & age < 50 ~ '40 ~ 50',
+    age >= 50 & age < 60 ~ '50 ~ 60',
+    age >= 60 & age < 70 ~ '60 ~ 70',
+    age >= 70 ~ '>= 70'
+  ))
+
+# rename columns with the same content
+reduced_data<-rename(reduced_data, education=educd)
+reduced_data<-rename(reduced_data, state=stateicp)
+
+# choose those can vote
+reduced_data <-
+  reduced_data %>% 
+  filter(age >= 18 &
+           citizen != "n/a" &
+           citizen != "not a citizen")
+
+# unify race of two datasets
+reduced_data <-
+  reduced_data %>% mutate(race = case_when(
+    race=="other race, nec" |
+      race=="two major races" |
+      race=="three or more major races"~"other",
+    race=="black/african american/negro"~"black",
+    race=="chinese" |
+      race=="japanese" |
+      race=="other asian or pacific islander"~ "asian or pacific",
+    race=="american indian or alaska native" ~ "american indian or alaska native",
+    race=="white" ~ "white"
+  ))
+
+# unify the education of two datasets
+edu_atmost_high = c("1 or more years of college credit, no degree",
+                    "12th grade, no diploma", "regular high school diploma",
+                    "no schooling completed", "grade 10", "grade 11",
+                    "ged or alternative credential", "grade 9", "grade 8",
+                    "grade 7", "grade 6", "grade 5", "grade 4", "grade 3",
+                    "grade 2", "grade 1", "kindergarten")
+edu_bachelor = c("bachelor's degree", "some college, but less than 1 year")
+edu_grad = c("doctoral degree", "master's degree")
+edu_other_tertiary = c("associate's degree, type not specified",
+                       "professional degree beyond a bachelor's degree",
+                       "nursery school, preschool")
+
+# unify education of multiple datasets
+reduced_data<-
+  reduced_data %>% mutate(
+    education=case_when(
+      education %in% edu_atmost_high ~ "at most high school",
+      education %in% edu_bachelor ~ "bachelor",
+      education %in% edu_grad ~ "graduate",
+      education %in% edu_other_tertiary ~ "tertiary (not bachelor)"
+    )
+  )
+
+# unify the state by the state code
+reduced_data<-
+  reduced_data %>% mutate(
+    state = case_when(
+      state=="alabama" ~ "AL",
+      state=="alaska" ~ "AK",
+      state=="arizona" ~ "AZ",
+      state=="arkansas" ~ "AR",
+      state=="california" ~ "CA",
+      state=="colorado" ~ "CO",
+      state=="connecticut" ~ "CT",
+      state=="delaware" ~ "DE",
+      state=="district of columbia" ~ "DC",
+      state=="florida" ~ "FL",
+      state=="georgia" ~ "GA",
+      state=="hawaii" ~ "HI",
+      state=="idaho" ~ "ID",
+      state=="illinois" ~ "IL",
+      state=="indiana" ~ "IN",
+      state=="iowa" ~ "IA",
+      state=="kansas" ~ "KS",
+      state=="kentucky" ~ "KY",
+      state=="louisiana" ~ "LA",
+      state=="maine" ~ "ME",
+      state=="maryland" ~ "MD",
+      state=="massachusetts" ~ "MA",
+      state=="michigan" ~ "MI",
+      state=="minnesota" ~ "MN",
+      state=="mississippi" ~ "MS",
+      state=="missouri" ~ "MO",
+      state=="montana" ~ "MT",
+      state=="nebraska" ~ "NE",
+      state=="nevada" ~ "NV",
+      state=="new hampshire" ~ "NH",
+      state=="new jersey" ~ "NJ",
+      state=="new mexico" ~ "NM",
+      state=="new york"~ "NY",
+      state=="north carolina" ~ "NC",
+      state=="north dakota" ~ "ND",
+      state=="ohio" ~ "OH",
+      state=="oklahoma" ~ "OK",
+      state=="oregon" ~ "OR",
+      state=="pennsylvania" ~ "PA",
+      state=="rhode island" ~ "RI",
+      state=="south carolina" ~ "SC",
+      state=="south dakota" ~ "SD",
+      state=="tennessee" ~ "TN",
+      state=="texas" ~ "TX",
+      state=="utah" ~ "UT",
+      state=="vermont" ~ "VT",
+      state=="virginia" ~ "VA",
+      state=="washington" ~ "WA",
+      state=="west virginia" ~ "WV",
+      state=="wisconsin" ~ "WI",
+      state=="wyoming" ~ "WY"
+    )
+  )
+
+# del all NA if there is
+reduced_data <- na.omit(reduced_data)
 
 # Saving the census data as a csv file in my
 # working directory
--- 01-data_cleaning-survey1.R
+++ 01-data_cleaning-survey1.R
@@ -17,7 +17,7 @@ library(tidyverse)
 setwd(".")
 # Read in the raw data (You might need to change this if you use a different dataset)
 # raw_data <- read_dta("inputs/ns20200625/ns20200625.dta")
-raw_data <- read_dta("ns20200625/ns20200625.dta")
+raw_data <- read_dta("../STA304/now/question_set/Qset3/raw_data/ns20200625/ns20200625.dta")
 # Add the labels
 raw_data <- labelled::to_factor(raw_data)
 # Just keep some variables
@@ -47,10 +47,87 @@ reduced_data <-
 # Maybe check the values?
 # Is vote a binary? If not, what are you going to do?
 
+reduced_data$inctot <- as.integer(reduced_data$inctot)
+reduced_data$perwt <- as.numeric(reduced_data$perwt)
+
+# create age category
+reduced_data$age <- as.integer(reduced_data$age)
+reduced_data = reduced_data %>%
+  mutate(age_group = case_when (
+    age >= 18 & age < 20 ~ '18 ~ 20',
+    age >= 20 & age < 30 ~ '20 ~ 30',
+    age >= 30 & age < 40 ~ '30 ~ 40',
+    age >= 40 & age < 50 ~ '40 ~ 50',
+    age >= 50 & age < 60 ~ '50 ~ 60',
+    age >= 60 & age < 70 ~ '60 ~ 70',
+    age >= 70 ~ '>= 70'
+  ))
+
 reduced_data<-
   reduced_data %>%
   mutate(vote_trump = 
            ifelse(vote_2020=="Donald Trump", 1, 0))
+
+# focus those who can and will vote and those who support Democrates
+# and Republicans only
+reduced_data<-
+  reduced_data %>%
+  filter(registration=="Registered" &
+           vote_intention=="Yes, I will vote" &
+           age >= 18 &
+           (vote_2016=="Donald Trump" |
+            vote_2016=="Hillary Clinton") &
+           (vote_2020=="Donald Trump" |
+           vote_2020=="Joe Biden"))
+
+# change all vairables to lowercase
+reduced_data$hispanic = tolower(reduced_data$hispanic)
+reduced_data$gender = tolower(reduced_data$gender)
+
+# rename columns with common contents
+reduced_data<-rename(reduced_data, sex=gender)
+reduced_data<-rename(reduced_data, hispan=hispanic)
+reduced_data<-rename(reduced_data, race=race_ethnicity)
+
+# focus on Hispanic group since there are more and more Hispanic people born in USA
+reduced_data<-reduced_data %>% 
+  mutate(hispan = ifelse(hispan=="not hispanic" |
+                           hispan=="mexican" |
+                           hispan=="cuban" |
+                           hispan=="puerto rican", hispan, "other"))
+
+# unify the race of two datasets
+reduced_data$race = tolower(reduced_data$race)
+lst_asian_pacific = c("asian (chinese)", "asian (japanese)", "asian (filipino)",
+              "asian (vietnamese)", "asian (korean)", "asian (asian indian)",
+              "asian (other)", "pacific islander (guamanian)", "pacific islander (native hawaiian)",
+              "pacific islander (other)", "pacific islander (samoan)")
+reduced_data<-reduced_data %>%
+  mutate(race=case_when(
+    race == "black, or african american"~"black",
+    race %in% lst_asian_pacific ~ "asian or pacific",
+    race == "some other race"~"other",
+    race == "american indian or alaska native" ~ "american indian or alaska native",
+    race == "white" ~ "white"
+  ))
+
+# unify education degree
+reduced_data <-
+  reduced_data %>%
+  mutate(education=case_when(
+    education=="Completed some college, but no degree" |
+      education=="College Degree (such as B.A., B.S.)" ~ "bachelor",
+    education=="Other post high school vocational training" |
+      education=="Associate Degree"~ "tertiary (not bachelor)",
+    education=="Doctorate degree" |
+      education=="Masters degree" |
+      education=="Completed some graduate, but no degree" ~ "graduate",
+    education=="High school graduate" |
+      education=="Completed some high school"~"at most high school"
+  ))
+
+# del all NA if there is
+reduced_data <- na.omit(reduced_data)
 
 # Saving the survey/sample data as a csv file in my
 # working directory
